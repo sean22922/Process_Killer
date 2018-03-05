36d2bb3c9bb5e298977d7ba599c5c27b52ba2fed
sean22922 <sean22922@ymail.com>
2018-03-05 18:12:38 +0800

    Featuring multiple language & some fix.
    There's only two language... how can you say this  is "multiple" language.

--- Process_Killer(exe).bat
+++ Process_Killer(exe).bat
@@ -1,30 +1,87 @@
 @echo off
 
+
+FOR /F "tokens=3 delims= " %%G in ('reg query "hklm\system\controlset001\control\nls\language" /v Installlanguage') DO (
+IF [%%G] EQU [0404] (
+  goto main_zhtw
+) ELSE (
+  goto main_en
+)
+
+:main_zhtw
 echo 歡迎使用本程式！
 echo 本程式 20 秒後將自動終止學生端處理程序
 echo 1. 終止學生端處理程序
 echo 2. 自行輸入要終止的處理程序
 echo 3. 離開
 choice /T 20 /C 123 /D 1 /M "請輸入選項"
-if errorlevel 3 goto 3
-if errorlevel 2 goto 2 
-if errorlevel 1 goto 1
-cls
+if errorlevel 3 goto 3_zhtw
+if errorlevel 2 goto 2_zhtw
+if errorlevel 1 goto 1_zhtw
 
-:1
+:main_en
+echo Thank you for using this program！
+echo This program will automaticly terminate StudentMain.exe 20 seconds later.
+echo 1. Terminate StudentMain.exe.
+echo 2. Enter the process name you want to terminate.
+echo 3. Leave
+choice /T 20 /C 123 /D 1 /M "Please choose an option"
+if errorlevel 3 goto 3_en
+if errorlevel 2 goto 2_en
+if errorlevel 1 goto 1_en
+
+:1_zhtw
 cls
 taskkill /im StudentMain.exe /f >nul 2>&1
-if errorlevel 1 (echo 處理程式中止失敗...) else (echo 處理程序中止成功...)
+if errorlevel 1 (
+echo 處理程式中止失敗...
+echo 也許處理程序原本就不存在? 或是我沒拿到系統管理權限?
+) else (
+echo 處理程序中止成功...
+)
 goto 3
 
-:2
+:2_zhtw
 cls
-set /p processname=處理程序名稱:
-taskkill /im %processname% /f >nul 2>&1
-if errorlevel 1 (echo 處理程式中止失敗...) else (echo 處理程序中止成功...)
+set /p processname=輸入你想要終止的處理程序名稱(不包含結尾的.exe):
+taskkill /im %processname%.exe /f >nul 2>&1
+if errorlevel 1 (
+echo 處理程式中止失敗...
+echo 也許處理程序原本就不存在? 或是我沒拿到系統管理權限?
+) else (
+echo 處理程序中止成功...
+)
 goto 3
 
-:3
-echo 按任意鍵關閉視窗
+:3_zhtw
+echo 按任意鍵關閉視窗...
+pause >nul
+exit
+
+:1_en
+cls
+taskkill /im StudentMain.exe /f >nul 2>&1
+if errorlevel 1 (
+echo Failed to terminate process... 
+echo Maybe the process doesn't even exist? or i didn't get admin privileges?
+) else (
+echo Process terminated sucessfully...
+)
+goto 3
+
+:2_en
+cls
+set /p processname=The process name you want to terminate(don't enter .exe):
+taskkill /im %processname%.exe /f >nul 2>&1
+if errorlevel 1 (
+echo Failed to terminate process... 
+echo Maybe the process doesn't even exist? or i didn't get admin privileges?
+) else (
+echo Process terminated sucessfully...
+)
+goto 3
+
+:3_en
+echo Press any botton to close this window....
 pause >nul
 exit
